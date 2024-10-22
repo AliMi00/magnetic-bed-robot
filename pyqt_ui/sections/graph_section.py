93d2ba46a62ab11b8c8a4299be724095f9d53b7c
Ahmed Al-Ganad <ahmedbaligh100@gmail.com>
2024-10-22 13:16:50 +0200

    Real-time graph axes made adjustable for the user with a .yaml file at config/param_set_axes.yaml

--- pyqt_ui/sections/graph_section.py
+++ pyqt_ui/sections/graph_section.py
@@ -7,13 +7,24 @@ import time
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 import pandas as pd
-
+import yaml
 
 class GraphSection(QWidget):
     def __init__(self, config_manager, parent=None):
         super().__init__(parent)
         self.config = config_manager
+        self.num_axes = self.load_axes("config/param_set_axes.yaml")
         self.init_ui()
+
+    def load_axes(self, filename):
+        """Load the number of axes to plot from .yaml file"""
+        with open(filename, "r") as file:
+            data = yaml.safe_load(file)
+            num_axes = data.get('axes', 3)  # Default to 3 if  not specified
+            if num_axes not in [2, 3]:
+                raise ValueError("Axes value must be either 2 or 3.")
+            return num_axes
+
 
     def init_ui(self):
         """Initialize the Graph Data Section."""
@@ -49,10 +60,15 @@ class GraphSection(QWidget):
             force_x = self.data['Force_M1.Force_x [newton]']
             force_y = self.data['Force_M1.Force_y [newton]']
             force_z = self.data['Force_M1.Force_z [newton]']
-            # Plot the force data
-            self.ax.plot(distance, force_x, label='Force_x', color='r')
-            self.ax.plot(distance, force_y, label='Force_y', color='g')
-            self.ax.plot(distance, force_z, label='Force_z', color='b')
+
+            # Plot data based on the number of axes data
+            if self.num_axes == 2:
+               self.ax.plot(distance, force_x, label='Force_x', color='r')
+               self.ax.plot(distance, force_y, label='Force_y', color='g')
+            elif self.num_axes == 3:
+               self.ax.plot(distance, force_x, label='Force_x', color='r')
+               self.ax.plot(distance, force_y, label='Force_y', color='g')
+               self.ax.plot(distance, force_z, label='Force_z', color='b')
 
             # Add labels, title, and legend
             self.ax.set_xlabel('Distance to steel plate [mm]')
